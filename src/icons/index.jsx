7b01add3a40410e04dccd0a0a001f1fd432c9f6c
Miguel Contreras <miguel.contreras@wizeline.com>
2020-09-20 20:52:58 -0500

    add icons

---
+++ src/icons/index.jsx
@@ -0,0 +1,5 @@
+import HamburguerMenuIcon from './list.svg';
+import SearchIcon from './magnifying-glass.svg';
+import UserIcon from './user.svg';
+
+export { HamburguerMenuIcon, SearchIcon, UserIcon };
