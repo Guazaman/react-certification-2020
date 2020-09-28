8ea65637ce4f23923b09b91f9994fc2bb5fd8ae0
Miguel Contreras <miguel.contreras@wizeline.com>
2020-09-28 18:56:07 -0500

    change youtube key

--- src/api/YoutubeApi.jsx
+++ src/api/YoutubeApi.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyC6huJeUUGMrYDoD1HGed4Xg8QQgSyzesY';
+const KEY = 'AIzaSyBqsKT2xXhdgSRUxibA0SVclsKYHyW15Kw';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3/',
