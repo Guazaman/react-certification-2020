920579205f3cc6a2356f0120ce222314a5418189
Miguel Contreras <miguel.contreras@wizeline.com>
2020-10-01 19:45:35 -0500

    Use Global styles from styled components

---
+++ src/GlobalStyles.jsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
+export default GlobalStyles;
