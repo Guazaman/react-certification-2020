c7553f6a00b16fdf95fde6c884f921b0eab81c87
Miguel Contreras <miguel.contreras@wizeline.com>
2020-10-04 14:01:16 -0500

    add private test

---
+++ src/components/Private/Private.test.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import PrivateComponent from './Private.component';
+import { useAuth } from '../../providers/Auth';
+
+jest.mock('../../providers/Auth', () => ({
+  useAuth: jest.fn(),
+}));
+
+const privateWrapper = (
+  <BrowserRouter>
+    <PrivateComponent>
+      <div>Some test component</div>
+    </PrivateComponent>
+  </BrowserRouter>
+);
+
+describe('Test private routes', () => {
+  it('Should render the component', () => {
+    useAuth.mockImplementation(() => {
+      return {
+        authenticated: true,
+      };
+    });
+    const wrapper = render(privateWrapper);
+    const mockComponent = wrapper.queryByText('Some test component');
+    expect(mockComponent).not.toBeNull();
+  });
+
+  it('Should not render the component', () => {
+    useAuth.mockImplementation(() => {
+      return {
+        authenticated: false,
+      };
+    });
+    const wrapper = render(privateWrapper);
+    const mockComponent = wrapper.queryByText('Some test component');
+    expect(mockComponent).toBeNull();
+  });
+});
