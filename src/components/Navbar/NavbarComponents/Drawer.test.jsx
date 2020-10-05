b217cd76772840691c6ad4b2b4bb2801ceda7620
Miguel Contreras <miguel.contreras@wizeline.com>
2020-10-05 01:59:16 -0500

    add some tests

--- src/components/Navbar/NavbarComponents/Drawer.test.jsx
+++ src/components/Navbar/NavbarComponents/Drawer.test.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter, Link } from 'react-router-dom';
+import { ListItemText, ListItemIcon } from '@material-ui/core';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import Drawer from './Drawer';
 
-it('Should render Drawer closed', () => {
-  const wrapper = render(<Drawer sideBarstate={false} toggleDrawer={jest.fn()} />);
-  expect(wrapper).not.toBeNull();
+describe('Test the Drawer closed', () => {
+  it('Should render Drawer closed', () => {
+    const wrapper = render(<Drawer sideBarstate={false} toggleDrawer={jest.fn()} />);
+    expect(wrapper).not.toBeNull();
+  });
 });
 
+describe('Test the Drawer open', () => {
+  const showFavorites = (
+    <Link to="/favorites">
+      <ListItemIcon>
+        <FavoriteIcon />
+      </ListItemIcon>
+      <ListItemText primary="Favorites" />
+    </Link>
+  );
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = render(
+      <BrowserRouter>
+        <Drawer sideBarstate toggleDrawer={jest.fn()} favoritesItem={showFavorites} />
+      </BrowserRouter>
+    );
+  });
+
+  it('Should render Drawer open', () => {
+    expect(wrapper).not.toBeNull();
+  });
+
+  it('Should have a link to Home', () => {
+    const text = wrapper.getByText('Home');
+    expect(text).not.toBeNull();
+  });
+
+  it('Should have a link to favorites', () => {
+    const text = wrapper.getByText('Favorites');
+    expect(text).not.toBeNull();
+  });
+});
--- src/providers/Auth/Auth.test.jsx
+++ src/providers/Auth/Auth.test.jsx
@@ -2,12 +2,12 @@ import { renderHook } from '@testing-library/react-hooks';
 import AuthProvider, { useAuth } from './Auth.provider';
 
 describe('Test auth provider', () => {
-  it('Should set up the context', () => {
+  it('Should set the context', () => {
     const { result } = renderHook(() => useAuth());
     expect(result).not.toBeNull();
   });
 
-  it('Should set up the provider', () => {
+  it('Should set the provider', () => {
     const { result } = renderHook(() => AuthProvider());
     expect(result).not.toBeNull();
   });
